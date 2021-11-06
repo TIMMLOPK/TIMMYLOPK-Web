82c803c3cbd7d2701dcff81766b84e125f47aa23
TIMMYLOPK <timmy.12345wer11@gmail.com>
2021-11-06 17:05:21 +0800

    [3.1.1]

--- pages/blog.js
+++ pages/blog.js
@@ -2,7 +2,7 @@ import fs from 'fs'
 import path from 'path'
 import matter from 'gray-matter'
 import Head from 'next/head'
-import Post from '../components/Post'
+import Post from '../components/post'
 import { sortByDate } from '../utils/day'
 
 
