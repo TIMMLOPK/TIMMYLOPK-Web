2066537d42b1f0db614b74bfe059db2cdfd4d00b
TIMMYLOPK <timmy.12345wer11@gmail.com>
2022-04-12 16:21:51 +0800

    v3.9.9(Patch.2)

--- pages/building.js
+++ pages/building.js
@@ -1,5 +1,6 @@
-import { Box, Text, SimpleGrid, Avatar} from '@chakra-ui/react'
+import { Box, Text, SimpleGrid, Avatar } from '@chakra-ui/react'
 import Image from 'next/image'
+import NextLink from 'next/link'
 
 const LogoMap = [
   {
@@ -39,33 +40,81 @@ const LogoMap = [
 const shop = () => {
   return (
     <Box>
-      <Image src="/shop/bg_cover.jpg" width="738" height="390" alt='cover Image'/>
-      <Box bg="white" height="100vh" width="738">
-        <Box color="black" marginTop="-1.5">
-          <SimpleGrid textAlign='right'>
-            <Text fontSize='xs'>
-              Illustration: Sun KAl
-            </Text>
-            <Box paddingLeft='70%'>
-              <Avatar src="shopicon.jpg" loading="lazy" size='sm' />
-              <Text display='inline-block' alignItems='center' margin='auto' fontSize='5px'>@murasaki_jpstore</Text>
+      <Box
+        position='relative'
+        h={400}
+        _after={{
+          content: '""',
+          bgImage:
+            "url(/shop/bg_cover.jpg)",
+          bgSize: "cover",
+          pos: "absolute",
+          top: 0,
+          right: 0,
+          left: 0,
+          bottom: 0,
+          zIndex: -1
+        }}>
+        <Text fontSize='0.5px' zIndex={2} textColor='black' pos='absolute' bottom='0' right='0'>
+          Illustration: Sun KAl
+        </Text>
+      </Box>
+      <Box bg="white" height={{ base: '600px', md: '400px' }} width="738">
+        <Box color="black" >
+          <Box textAlign='right'>
+            <Avatar src="shopicon.jpg" loading="lazy" size='sm' />
+            <Box display='inline-flex' alignItems='center'>
+              <Text fontStyle='italic' fontSize='x-small' marginLeft={2} marginRight={2}>@murasaki_jpstore</Text>
+              <Image src='/shop/carosell.svg' width='20px' height='20px' alt='carosell'/>
+              <Image src='/shop/ig.svg' width='25px' height='25px' alt='ins'/>
             </Box>
-          </SimpleGrid>
-          <Text>日本代購</Text>
-          <Text>出團 78-82算 運費平分</Text>
-          <Text>買voice 73算(少量)75算</Text>
-          <Text>代購/中古 8-85算 包括但不限於：</Text>
-          <Box paddingLeft="60px" paddingTop="5%">
+          </Box>
+          <Box>
+            <Text>日本代購</Text>
+            <Text>出團 78-82算 運費平分</Text>
+            <Text>買voice 73算(少量)75算</Text>
+            <Text>代購/中古 8-85算 包括但不限於：</Text>
+          </Box>
+          <Box paddingTop="5%" textAlign='center'>
             <SimpleGrid columns={[2, null, 4]} spacingX="40px" spacingY="20px">
               {LogoMap.map(item => (
                 <Box key={item.id} w="100%">
-                  <Image src={item.src} width="100px" height="100px" alt='logo'/>
+                  <Image src={item.src} width="100px" height="100px" alt='logo' />
                 </Box>
               ))}
             </SimpleGrid>
           </Box>
         </Box>
-      </Box >
+      </Box>
+      <Box
+        position='relative'
+        h={400}
+        _after={{
+          content: '""',
+          bgImage:
+            "url(/shop/bg.png)",
+          bgSize: "cover",
+          pos: "absolute",
+          top: 0,
+          right: 0,
+          left: 0,
+          bottom: 0,
+          zIndex: -1
+        }}>
+        <Box margin='auto'>
+          <Text padding='15px'>傳送門</Text>
+          <SimpleGrid columns={[3, null, 5]} padding='15px'>
+            <NextLink href='#1'>代購須知</NextLink>
+            <NextLink href='#2'>付款方式</NextLink>
+            <NextLink href='#3'>交收方式</NextLink>
+            <NextLink href='#4'>運送時效</NextLink>
+            <NextLink href='#5'>風險披霹</NextLink>
+          </SimpleGrid>
+        </Box>
+        <Box id='1' padding='15px'>
+          <Text as='h1'>代購須知</Text>
+        </Box>
+      </Box>
     </Box >
   )
 }
